b7b69c300c1185d1ad56596413fe2574ed275d3d
1998-felix <felix.gateru@gmail.com>
2023-01-25 19:22:08 +0300

    Establish WiFi connection

---
+++ OTA_client/include/credentials.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+struct{
+    const char* host_name = "Esp32";
+    const char* ssid = "OH_LAN";
+    const char* password = "99998888";
+}my_credentials;
+
+
+
+
+
+#endif
--- OTA_client/src/main.cpp
+++ OTA_client/src/main.cpp
@@ -1,9 +1,67 @@
 #include <Arduino.h>
+#include <Update.h>
+#include <WiFi.h>
+
+#include "credentials.h"
+
+// function prototypes
+void scanNetworks();
+void connnectWiFi();
+void displayWiFiDetails();
 
 void setup() {
-  // put your setup code here, to run once:
-}
+  // set up serial
+  Serial.begin(115200);
 
+  // WI-FI Protocols
+  // 1. Scan and display networks
+  scanNetworks();
+  // 2. Connect to network
+  connnectWiFi();
+  // 3. List wi-fi connection details
+  displayWiFiDetails();
+
+  // Built in LED
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  delay(1000);
+}
+void scanNetworks() {
+  Serial.println(".................");
+  Serial.println("Scanning networks");
+  Serial.println(".................");
+  byte num_ssid = WiFi.scanNetworks();
+  for (int i = 0; i < num_ssid; i++) {
+    Serial.print("Network # ");
+    Serial.printf(" %d ", i);
+    Serial.println(WiFi.SSID(i));
+  }
+  Serial.println(".................");
+  Serial.println("Scan complete....");
+  Serial.println("..................");
+}
+void connnectWiFi() {
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf("Attempting to connect to: %s \n", my_credentials.ssid);
+    WiFi.begin(my_credentials.ssid, my_credentials.password);
+    delay(3000);
+    if (WiFi.status() == WL_IDLE_STATUS) {
+      Serial.println("Attempting connection. Device Idle ");
+    }
+  }
+  Serial.println("Wifi connected succesfully!!!!!");
+}
+void displayWiFiDetails() {
+  long rssi = WiFi.RSSI();
+  IPAddress ip;
+  ip = WiFi.localIP();
+  Serial.print("Wifi Network: ");
+  Serial.print(my_credentials.ssid);
+  Serial.print(" RSSI : ");
+  Serial.print(rssi);
+  Serial.println(" dbm");
+  Serial.print("Device Ip Address: ");
+  Serial.println(ip);
 }
