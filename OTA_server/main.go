2f0f4e9aa41081e54708f97fb82ea2038b831095
1998-felix <felix.gateru@gmail.com>
2023-01-26 21:36:21 +0300

    Update server

--- OTA_server/main.go
+++ OTA_server/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var staticDir = filepath.FromSlash("/home/oh/Documents/Projects/OTA_File_Handler/OTA_server/static")
+
+func handleBin(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join(staticDir, "/cv.pdf"))
+}
+
+func handlehtml(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join(staticDir, "/index.html"))
+}
+
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+
+	http.HandleFunc("/bin", handleBin)
+	http.HandleFunc("/", handlehtml)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
