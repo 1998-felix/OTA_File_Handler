068bde4ae9923fa4592f9049ab976cf71e9923c3
1998-felix <felix.gateru@gmail.com>
2023-01-28 17:09:53 +0300

    ⚡️ Add Logs to Server

--- OTA_server/main.go
+++ OTA_server/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
+// To fix this for any server
 var staticDir = filepath.FromSlash("/home/oh/Documents/Projects/OTA_File_Handler/OTA_server/static")
+var total_index_requests int = 0
+var total_bin_requests int = 0
 
 func handleBin(w http.ResponseWriter, r *http.Request) {
+	total_bin_requests += 1
+	log.Printf("Download request # %v made", total_bin_requests)
 	http.ServeFile(w, r, filepath.Join(staticDir, "/firmware.bin"))
 }
 
 func handlehtml(w http.ResponseWriter, r *http.Request) {
+	total_index_requests += 1
+	log.Printf("Download request # %v made", total_index_requests)
 	http.ServeFile(w, r, filepath.Join(staticDir, "/index.html"))
 }
 
@@ -21,7 +27,7 @@ func main() {
 
 	http.HandleFunc("/bin", handleBin)
 	http.HandleFunc("/", handlehtml)
-	fmt.Println("Starting server at port 8080")
+	log.Println("Starting the server at port 8080 ")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
